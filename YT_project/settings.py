86fc7e75f9a12bb168f96efc8a0a94a51b5aee17
hemanth <hemanthpolugalla@gmail.com>
2021-08-24 16:03:46 +0530

    updated local host

--- YT_project/settings.py
+++ YT_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nam)vaw8c!hi%gckalxdxlw24n=)f1-c3r++y7ji-cy5!##5(@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['video-dowmloader.herokuapp.com']
 
 
 # Application definition
