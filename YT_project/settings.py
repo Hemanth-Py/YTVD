d55d1b91387e4f5af26faa44af4bfd9f60995685
Hemanth P <hemanth@konfhub.com>
2026-04-05 07:58:35 +0530

    fix: replace hardcoded Django SECRET_KEY with env var

--- YT_project/settings.py
+++ YT_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nam)vaw8c!hi%gckalxdxlw24n=)f1-c3r++y7ji-cy5!##5(@'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-nam)vaw8c!hi%gckalxdxlw24n=)f1-c3r++y7ji-cy5!##5(@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
